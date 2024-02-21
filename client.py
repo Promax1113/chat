2dbe05b777e2bc3823c9ef034a567cacbef6f3d6
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2024-02-21 10:37:38 +0100

    Add receive function to client

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import socket
 import logging
 import choice
 import configparser
+from typing import Final
+
+BUFSIZE: Final[int] = 4096
 
 logger = logging.getLogger('client-info')
 logger.setLevel(logging.INFO)
@@ -12,69 +15,74 @@ logger.addHandler(ch)
 
 
 class User:
-	username: str
-	sock: socket.socket
+    username: str
+    sock: socket.socket
 
-	def __init__(self, _socket, _username=os.getlogin(), password_hash=None):
-		self.sock = _socket
-		self.username = _username
+    def __init__(self, _socket, _username=os.getlogin(), password_hash=None):
+        self.sock = _socket
+        self.username = _username
 
-	def __pre_login(self):
-		client_data = {"username": self.username}
+    def __pre_login(self):
+        client_data = {"username": self.username,
+                       "key_hash": choice.Input("Enter the password for the server connection (max 20 char)").ask()}
+        
+    def receive(self, decode: bool = True):
+        message = self.sock.recv(BUFSIZE)
+        return message.decode() if decode else message
 
-	def handle_login(self):
-		self.__pre_login()
+    def handle_login(self):
+        self.__pre_login()
 
 
 def pre_connect(config: configparser.ConfigParser, _config_file: str):
-	config.read(_config_file)
-	if os.path.exists(_config_file) and "connection.details" in config.sections():
-		return config['connection.details']['ip_address'], config['connection.details']['port']
-	_ip, _port = choice.Input("IP address to connect to").ask(), choice.Input("Port of the address", int).ask()
-	config['connection.details'] = {'ip_address': _ip, 'port': _port}
-	with open(_config_file, "w") as file:
-		config.write(file)
-	return _ip, _port
+    config.read(_config_file)
+    if os.path.exists(_config_file) and "connection.details" in config.sections():
+        return config['connection.details']['ip_address'], config['connection.details']['port']
+    _ip, _port = choice.Input("IP address to connect to").ask(), choice.Input("Port of the address", int).ask()
+    config['connection.details'] = {'ip_address': _ip, 'port': _port}
+    with open(_config_file, "w") as file:
+        config.write(file)
+    return _ip, _port
 
 
 # add connection retries.
 def connect(ip, port, mode="nogui") -> socket.socket:
-	"""
-	Connects client to specified ip and port using TCP and IPv4.
-	"""
-	print(f"Connecting to {ip}:{port}...")
-	_client = socket.socket(family=socket.AF_INET, proto=socket.IPPROTO_TCP)
-	_client.connect((ip, port))
-	return _client
+    """
+    Connects client to specified ip and port using TCP and IPv4.
+    """
+    print(f"Connecting to {ip}:{port}...")
+    _client = socket.socket(family=socket.AF_INET, proto=socket.IPPROTO_TCP)
+    _client.connect((ip, port))
+    return _client
 
 
 if __name__ == "__main__":
-	choices: list = ["Connect to server", "Change username", "Change saved IP Address"]
-	config_file = "cconfig.cfg"
-	config = configparser.ConfigParser()
-	config.read(config_file)
-	if os.path.isfile(config_file) and "user" in config.sections():
-		username = config['user']['username']
-	else:
-		username = os.getlogin()
-	ip, port = None, None
-	while ip is None and port is None:
-		print(f"\nCurrently using username: {username}\n")
+    choices: list = ["Connect to server", "Change username", "Change saved IP Address"]
+    config_file = "cconfig.cfg"
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    if os.path.isfile(config_file) and "user" in config.sections():
+        username = config['user']['username']
+    else:
+        username = os.getlogin()
+    ip, port = None, None
+    while ip is None and port is None:
+        print(f"\nCurrently using username: {username}\n")
 
-		match choice.Menu(choices, title="Action menu:").ask():
-			case "Connect to server":
-				ip, port = pre_connect(config, config_file)
+        match choice.Menu(choices, title="Action menu:").ask():
+            case "Connect to server":
+                ip, port = pre_connect(config, config_file)
 
-			case "Change username":
-				username = choice.Input("New username (limit 12 char)").ask()[:12].replace(" ", "_")
-				config['user'] = {'username': username}
-				with open(config_file, "w") as file:
-					config.write(file)
-			case "Change saved IP Address":
-				logger.info("Removing entry...")
-				config.remove_section("connection.details")
-				with open(config_file, "w") as f:
-					config.write(f)
-				ip, port = pre_connect(config, config_file)
-	client: User = User(connect(ip, int(port)), _username=username)
-	client.handle_login()
+            case "Change username":
+                username = choice.Input("New username (limit 12 char)").ask()[:12].replace(" ", "_")
+                config['user'] = {'username': username}
+                with open(config_file, "w") as file:
+                    config.write(file)
+            case "Change saved IP Address":
+                logger.info("Removing entry...")
+                config.remove_section("connection.details")
+                with open(config_file, "w") as f:
+                    config.write(f)
+                ip, port = pre_connect(config, config_file)
+    client: User = User(connect(ip, int(port)), _username=username)
+    client.handle_login()
